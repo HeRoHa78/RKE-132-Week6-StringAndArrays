4190da0c6cde4fab92fff1eb94efcd45cff3c043
Helvi <helviroman@gmail.com>
2023-10-31 17:38:37 +0200

    add week6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+string[] snacks = { "sushi", "pizza", "burger", "chicken", "Asian takeaway" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are poing to eat {snacks[randomIndex]}.");
